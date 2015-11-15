19e279c784583b085bbe7fa52e5eac8f57b3d173
Katie Jackson <katiejackson@Katies-MacBook-Pro.local>
2015-11-15 14:47:19 -0500

    added magazine, contains,published_by, and review tables

--- schema.sql
+++ schema.sql
@@ -22,5 +22,11 @@ create table if not exists purchased_by(date_of_purchase date, customer_id integ
 
 create table if not exists written_by(author_id integer, book_id integer, primary key(author_id,book_id), foreign key(author_id) references author(person_id), foreign key(book_id) references book(isbn));
 
+create table if not exists magazine(issn integer, pub_date date, item_id integer, primary key(issn,pub_date), foreign key(item_id) references item(item_id));
 
+create table if not exists contains(order_id integer, item_id integer, primary key(order_id,item_id), foreign key(order_id) references c_order(order_number), foreign key(item_id) references item(item_id));
+
+create table if not exists published_by(publisher_id integer, item_id integer, primary key(publisher_id, item_id), foreign key(publisher_id) references publisher(pub_id), foreign key(item_id) references item(item_id));
+
+create table if not exists review(review_id integer primary key autoincrement not null, rating integer, body text, item_id integer, reviewer_id integer, foreign key(item_id) references item(item_id), foreign key(reviewer_id) references customer(person_id));
 
