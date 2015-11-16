d10a2bef08cacb005ccae6efee98c272f91734eb
Katie Jackson <katiejackson@Katies-MacBook-Pro.local>
2015-11-15 20:31:19 -0500

    Fixed purchased_by schema and populated database

--- schema.sql
+++ schema.sql
@@ -18,7 +18,7 @@ create table if not exists c_order(order_number integer primary key not null);
 
 create table if not exists customer(person_id integer primary key not null, email text, address text, phone_number text, credit_card integer, order_id integer, foreign key(order_id) references c_order(order_number), foreign key(person_id) references person(person_id));
 
-create table if not exists purchased_by(date_of_purchase date, customer_id integer, item_id integer, primary key(customer_id,item_id), foreign key(customer_id) references customer(person_id), foreign key(item_id) references item(item_id));
+create table if not exists purchased_by(date_of_purchase date, customer_id integer, item_id integer, primary key(date_of_purchase,customer_id,item_id), foreign key(customer_id) references customer(person_id), foreign key(item_id) references item(item_id));
 
 create table if not exists written_by(author_id integer, book_id integer, primary key(author_id,book_id), foreign key(author_id) references author(person_id), foreign key(book_id) references book(isbn));
 
